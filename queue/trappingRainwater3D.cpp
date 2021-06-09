463f0097a725ca7af752e1a559efae271f363be0
zee <zeeshanshaikh1205@gmail.com>
2021-06-09 16:46:55 -0400

    Minor changes

--- queue/trappingRainwater3D.cpp
+++ queue/trappingRainwater3D.cpp
@@ -10,39 +10,51 @@ using namespace std;
 class Solution {
 public:
     int trapRainWater(int heightMap[3][6]) {
-        r = 3; c = 6;
-        vector<vector<bool>> seen(r, vector<bool>(c));
-        for (int i = 0; i < r; i++) {
-            pq.push({heightMap[i][0], i * c});
-            pq.push({heightMap[i][c - 1], i * c + c - 1});
+        rows = 3; columns = 6;
+        vector<vector<bool>> seen(rows, vector<bool>(columns));
+        for (int i = 0; i < rows; i++) {
+            pq.push({heightMap[i][0], i * columns});
+            pq.push({heightMap[i][columns - 1], i * columns + columns - 1});
         }
-        for (int j = 1; j < c - 1; j++) {
+        for (int j = 1; j < columns - 1; j++) {
             pq.push({heightMap[0][j], j});
-            pq.push({heightMap[r - 1][j], (r - 1) * c + j});
+            pq.push({heightMap[rows - 1][j], (rows - 1) * columns + j});
         }
         int res = 0;
         while (!pq.empty()) {
             auto p = pq.top(); pq.pop();
-            dfs(p.second / c, p.second % c, p.first, heightMap, seen, res);
+            dfs(p.second / columns, p.second % columns, p.first, heightMap, seen, res);
         }
         return res;
     }
 
 private:
-    int r, c;
+    int rows, columns;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 
-    void dfs(int i, int j, int h, int heightMap[3][6], vector<vector<bool>>& seen, int& res) {
-        seen[i][j] = true;
-        if (heightMap[i][j] > h) {
-            pq.push({heightMap[i][j], i * c + j});
+    void dfs(int row, int column, int value, int heightMap[3][6], vector<vector<bool>>& seen, int& output) {
+        seen[row][column] = true;
+        if (heightMap[row][column] > value) {
+            pq.push({heightMap[row][column], row * columns + column});
             return;
         }
-        res += h - heightMap[i][j];
-        if (i > 0 && !seen[i - 1][j]) dfs(i - 1, j, h, heightMap, seen, res);
-        if (i < r - 1 && !seen[i + 1][j]) dfs(i + 1, j, h, heightMap, seen, res);
-        if (j > 0 && !seen[i][j - 1]) dfs(i, j - 1, h, heightMap, seen, res);
-        if (j < c - 1 && !seen[i][j + 1]) dfs(i, j + 1, h, heightMap, seen, res);
+
+        output += value - heightMap[row][column];
+
+        // Checking rows
+        if (row > 0 && !seen[row - 1][column]){
+            dfs(row - 1, column, value, heightMap, seen, output);
+        }
+        if (row < rows - 1 && !seen[row + 1][column]){
+            dfs(row + 1, column, value, heightMap, seen, output);
+        }
+        // Checking columns
+        if (column > 0 && !seen[row][column - 1]){
+            dfs(row, column - 1, value, heightMap, seen, output);
+        }
+        if (column < columns - 1 && !seen[row][column + 1]){
+            dfs(row, column + 1, value, heightMap, seen, output);
+        }
     }
 };
 
