4ef166a0a509ae92f7f57b261ba0f4f508bbaa09
zee <zeeshanshaikh1205@gmail.com>
2021-06-26 08:15:33 -0400

    Added BST

--- binarySearchTree/BST.cpp
+++ binarySearchTree/BST.cpp
@@ -7,19 +7,18 @@
 using namespace std;
 
 class Node{
-
+// Class Defining the nodes to be used in the tree
 public:
+    int key;
+    Node *right;
+    Node *left;
+
     Node(int value){
         // Constructor for the Node
         key = value;
         left = NULL;
         right = NULL;
     }
-
-    Node *right;
-    Node *left;
-// Class Defining the nodes to be used in the tree
-int key;
 };
 
 class BST{
@@ -28,16 +27,6 @@ public:
         // This function is used to set the key as root node for a tree
         Node *root = new Node(key);
         return root;
-    }
-
-    void Inorder(Node *root) {
-        // This function prints the tree in sorted order
-        if (!root){
-            return;
-        }
-        Inorder(root->left);
-        cout << root->key << " ";
-        Inorder(root->right);
     }
 
     Node * insert(Node *node, int key){
@@ -65,6 +54,54 @@ public:
         // This function is used to search for a key in the tree
     }
 
+    int countNodes(Node *root){
+        // A recursive function to count the number of nodes in the tree
+        // Time Complexity: O(N)
+        if(!root){
+            return 0;
+        }
+        else{
+            return (1 + countNodes(root->left) + countNodes(root->right));
+        }
+    }
+
+    int countLeafNodes(Node *root){
+        // A recursive function to count the number of leaf nodes in the tree
+        // Time Complexity: O(N)
+        if(root == NULL){
+            return 0;
+        }
+        if(root->left == NULL && root->right == NULL){
+            return 1;
+        }
+        else{
+            return (countLeafNodes(root->left) + countLeafNodes(root->right));
+        }
+    }
+
+    int countNonLeafNodes(Node *root){
+        // A recursive function to count the number of non-leaf nodes in the tree
+        // Time Complexity: O(N)
+        if(root == NULL){
+            return 0;
+        }
+        if(root->left == NULL && root->right == NULL){
+            return 0;
+        }
+        else{
+            return (1 + countNonLeafNodes(root->left) + countNonLeafNodes(root->right));
+        }
+    }
+
+    void Inorder(Node *root) {
+        // This function prints the tree in sorted order
+        if (!root){
+            return;
+        }
+        Inorder(root->left);
+        cout << root->key << " ";
+        Inorder(root->right);
+    }
 };
 
 int main(){
@@ -79,7 +116,20 @@ int main(){
     tree.insert(root, 55);
     tree.insert(root, 20);
 
+    // Printing the number of nodes in the tree
+    int count = tree.countNodes(root);
+    cout << "The total number of nodes in the tree are: " << count << endl;
+
+    // Printing the number of leaf nodes in the tree
+    int leafCount = tree.countLeafNodes(root);
+    cout << "The total number of leaf nodes in the tree are: " << leafCount << endl;
+
+    // Printing the number of non-leaf nodes in the tree
+    int NonLeafCount = tree.countNonLeafNodes(root);
+    cout << "The total number of non-leaf nodes in the tree are: " << NonLeafCount << endl;
+
     // Printing out the tree in-order
+    cout << "The values in the tree are: ";
     tree.Inorder(root);
     return 0;
 }
