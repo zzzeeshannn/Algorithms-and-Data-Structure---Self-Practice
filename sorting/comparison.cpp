f5a6951ce4705e8e903d755a45e20db25eaf914f
zzzeeshannn <zeeshanshaikh1205@gmail.com>
2021-12-20 05:14:55 -0500

    None

--- sorting/comparison.cpp
+++ sorting/comparison.cpp
@@ -3,10 +3,156 @@
 // This program is used to compare the performance of insertion, selection, tree and heap sort
 //
 
-# include "insertion_sort.cpp"
-# include "selection_sort.cpp"
-# include "heap_sort.cpp"
-# include "tree_sort.cpp"
+#include <iostream>
+
+using namespace std;
+// Tree sort and all the required functions here
+struct Node
+{
+    int key;
+    struct Node *left, *right;
+};
+
+// A utility function to create a new BST Node
+struct Node *newNode(int item)
+{
+    struct Node *temp = new Node;
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Stores inorder traversal of the BST
+// in arr[]
+void storeSorted(Node *root, int arr[], int &i)
+{
+    if (root != NULL)
+    {
+        storeSorted(root->left, arr, i);
+        arr[i++] = root->key;
+        storeSorted(root->right, arr, i);
+    }
+}
+
+/* A utility function to insert a new
+   Node with given key in BST */
+Node* insert(Node* node, int key)
+{
+    /* If the tree is empty, return a new Node */
+    if (node == NULL) return newNode(key);
+
+    /* Otherwise, recur down the tree */
+    if (key < node->key)
+        node->left  = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+
+    /* return the (unchanged) Node pointer */
+    return node;
+}
+
+// This function sorts arr[0..n-1] using Tree Sort
+void treeSort(int arr[], int n)
+{
+    struct Node *root = NULL;
+
+    // Construct the BST
+    root = insert(root, arr[0]);
+    for (int i=1; i<n; i++)
+        root = insert(root, arr[i]);
+
+    // Store inorder traversal of the BST
+    // in arr[]
+    int i = 0;
+    storeSorted(root, arr, i);
+}
+
+
+// Heap sort and the required functions here
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; // Initialize largest as root
+    int l = 2 * i + 1; // left = 2*i + 1
+    int r = 2 * i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+// Insertion sort here
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+        greater than key, to one position ahead
+        of their current position */
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Selection sort here
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+void main(){
+    int size_list1[1];
+    int size_list1[1];
+}
 
 
 
